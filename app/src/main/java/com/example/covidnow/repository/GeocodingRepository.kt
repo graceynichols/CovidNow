138b17dcbc3cd22664fd5c8e0c84565758ac11e0
Grace Nichols <graceynichols@fb.com>
2020-07-20 13:26:44 -0700

    Convert NewsRepository to Kotlin

--- app/src/main/java/com/example/covidnow/repository/GeocodingRepository.kt
+++ app/src/main/java/com/example/covidnow/repository/GeocodingRepository.kt
@@ -8,8 +8,10 @@ class GeocodingRepository {
     fun queryGeocodeLocation(x: Double, y: Double, apiKey: String, jsonHttpResponseHandler: JsonHttpResponseHandler?) {
         // Make API call to Google's GeoCoding API
         val client = AsyncHttpClient()
+        // Build URL for request using lat/lng
         val geoUrl = "$GEOCODE_URL$x,$y&key=$apiKey"
         Log.i(TAG, "Geo URL: $geoUrl")
+        // Make GET request
         client[geoUrl, jsonHttpResponseHandler]
     }
 

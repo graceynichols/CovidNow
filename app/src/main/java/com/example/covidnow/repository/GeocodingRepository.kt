71bc2f5acc6699fc9b579931168b351a5081fa94
Grace Nichols <graceynichols@fb.com>
2020-07-20 13:24:27 -0700

    Convert GeocodingRepository to Kotlin

---
+++ app/src/main/java/com/example/covidnow/repository/GeocodingRepository.kt
@@ -0,0 +1,20 @@
+package com.example.covidnow.repository
+
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+
+class GeocodingRepository {
+    fun queryGeocodeLocation(x: Double, y: Double, apiKey: String, jsonHttpResponseHandler: JsonHttpResponseHandler?) {
+        // Make API call to Google's GeoCoding API
+        val client = AsyncHttpClient()
+        val geoUrl = "$GEOCODE_URL$x,$y&key=$apiKey"
+        Log.i(TAG, "Geo URL: $geoUrl")
+        client[geoUrl, jsonHttpResponseHandler]
+    }
+
+    companion object {
+        private const val TAG = "GeocodingRepository"
+        const val GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+    }
+}

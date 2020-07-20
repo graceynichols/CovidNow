e14426d960d8d1780ee8c473bb0dbe5a8790d3d7
Grace Nichols <graceynichols@fb.com>
2020-07-20 12:21:19 -0700

    Changing login and parse repo files to Kotlin

---
+++ app/src/main/java/com/example/covidnow/activity/LoginActivity.kt
@@ -0,0 +1,96 @@
+package com.example.covidnow.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import com.example.covidnow.databinding.ActivityLoginBinding
+import com.example.covidnow.viewmodels.LoginViewModel
+import com.parse.ParseException
+import com.parse.ParseUser
+
+class LoginActivity : AppCompatActivity() {
+    private var mViewModel: LoginViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view: RelativeLayout? = binding?.root
+        setContentView(view)
+        mViewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        if (ParseUser.getCurrentUser() != null) {
+            Log.i(TAG, "Logging in: " + ParseUser.getCurrentUser().username)
+            goMainActivity()
+        }
+        binding?.btnLogin?.setOnClickListener {
+            Log.i(TAG, "onClick login button")
+            binding?.pbLoading?.visibility = ProgressBar.VISIBLE
+            val username = binding?.etUsername?.text.toString()
+            val password = binding?.etPassword?.text.toString()
+            mViewModel?.loginUser(username, password) { _, e ->
+                if (e != null) {
+                    Log.e(TAG, "Issue with login", e)
+                } else {
+                    goMainActivity()
+                }
+            }
+            binding?.pbLoading?.visibility = View.GONE
+        }
+        // On click listener for sign up button
+        binding?.btnSignup?.setOnClickListener(View.OnClickListener {
+            binding?.btnLogin?.visibility = View.GONE
+            binding?.etEmail?.visibility = View.VISIBLE
+            Log.i(TAG, "onClick signup button")
+            binding?.btnSignup?.setOnClickListener(View.OnClickListener {
+                val username = binding?.etUsername?.text.toString()
+                val password = binding?.etPassword?.text.toString()
+                val email = binding?.etEmail?.text.toString()
+                binding?.pbLoading?.visibility = ProgressBar.VISIBLE
+                // Make sure username and password pass basic requirements
+                if (username == "") {
+                    Toast.makeText(applicationContext, "Username missing!", Toast.LENGTH_SHORT).show()
+                    return@OnClickListener
+                }
+                if (password == "") {
+                    Toast.makeText(applicationContext, "Password missing!", Toast.LENGTH_SHORT).show()
+                    return@OnClickListener
+                }
+                // Signup user
+                mViewModel?.signupUser(username, password, email) { e ->
+                    if (e == null) {
+                        // Hooray! Let them use the app now.
+                        binding?.pbLoading?.visibility = ProgressBar.INVISIBLE
+                        Toast.makeText(applicationContext, "Successful sign up!", Toast.LENGTH_SHORT).show()
+                        goMainActivity()
+                    } else {
+                        // Sign up didn't succeed. Look at the ParseException
+                        // to figure out what went wrong
+                        binding?.pbLoading?.visibility = ProgressBar.INVISIBLE
+                        if (e.code == ParseException.USERNAME_TAKEN) {
+                            Toast.makeText(applicationContext, "Username already taken", Toast.LENGTH_SHORT).show()
+                        } else if (e.code == ParseException.INVALID_EMAIL_ADDRESS) {
+                            Toast.makeText(applicationContext, "Invalid Email", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(applicationContext, "Error while signing up", Toast.LENGTH_SHORT).show()
+                        }
+                        Log.i(TAG, e.toString())
+                    }
+                }
+            })
+        })
+    }
+
+    private fun goMainActivity() {
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+
+    companion object {
+        const val TAG = "LoginActivity"
+        private var binding: ActivityLoginBinding? = null
+    }
+}
---
+++ app/src/main/java/com/example/covidnow/viewmodels/LoginViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.covidnow.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.example.covidnow.repository.ParseRepository
+import com.parse.LogInCallback
+import com.parse.ParseUser
+import com.parse.SignUpCallback
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+    private val parseRepository: ParseRepository
+    fun loginUser(username: String, password: String?, logInCallback: LogInCallback?) {
+        Log.i(TAG, "Attempting to login user $username")
+        ParseUser.logInInBackground(username, password, logInCallback)
+    }
+
+    fun signupUser(username: String, password: String?, email: String?, signUpCallback: SignUpCallback?) {
+        Log.i(TAG, "Attempting to signup user $username")
+        parseRepository.createNewUser(username, password, email, signUpCallback)
+    }
+
+    companion object {
+        private const val TAG = "LoginViewModel"
+    }
+
+    init {
+        parseRepository = ParseRepository()
+    }
+}
